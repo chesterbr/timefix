e83ddf2548bab72369be1512fad0077c111c4e37
chesterbr <chesterbr@efead73c-1b2a-0410-96fe-3316d00283b9>
2007-02-23 02:47:13 +0000

    Initial version

---
+++ tags/1.0/src/timefix.java
@@ -0,0 +1,16 @@
+import br.blog.chester.timefix.Timefix;
+
+/**
+ * This class is just a "shortcut" to the real <code>Timefix</code>.
+ * <p>
+ * It exists just to make "timefix injection" easier - thanks to it, you just
+ * have to add timefix.jar to the <code>CLASSPATH</code> and insert "timefix"
+ * before the main class on the java call.
+ * 
+ * @author Chester
+ * 
+ * @see br.blog.chester.timefix.Timefix
+ */
+public class timefix extends Timefix {
+
+}
